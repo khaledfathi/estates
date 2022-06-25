e14ec339129ee934b9e2a9ede1b9e2f4119c47f1
khaledfathi <khaledfathi@protonmail.com>
2022-06-26 01:49:15 +0200

    upload the code

---
+++ mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowIcon(QIcon(":/icon/icon/app_icon.ico"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
